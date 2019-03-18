4ddb03f80d5c42b37a1ae495fea464258652657f
Jonathan Perry <jpper@umich.edu>
2019-03-18 14:57:45 +0900

    Added check for non-matrices

--- Ruby/Matrixrotation.rb
+++ Ruby/Matrixrotation.rb
@@ -2,7 +2,14 @@ require 'pp'
 
 class Matrixrotation
 
+  def all_elements_arrays_of_same_size?(a)
+    a.all? { |e| e.class==Array } && a.map(&:size).uniq.size == 1
+  end
+
   def testRotation?(origArray, rotatedArray)
+    if !all_elements_arrays_of_same_size?(origArray)
+      abort("Inputted object is not an matrix")
+    end
     Integer n = origArray.length # n=3
     i=0
     origArray.each do |array|
@@ -21,6 +28,9 @@ class Matrixrotation
 
   def rotateCounterclockwise(arrayToRotate)
     # Implementation of the counter-clockwise rotation
+    if !all_elements_arrays_of_same_size?(arrayToRotate)
+      abort("Inputted object is not an matrix")
+    end
     return arrayToRotate.transpose.reverse
   end
 
@@ -29,9 +39,11 @@ end
 a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 a_rot = [[3, 6, 9], [2, 5, 8], [1, 4, 7]]
 b = [[2, 4], [1, 3]]
+c = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
+c_rot = [[4, 8, 12, 16], [3, 7, 11, 15], [2, 6, 10, 14], [1, 5, 9, 13]]
 matrixclass = Matrixrotation.new
+pp matrixclass.rotateCounterclockwise(b)
 pp matrixclass.testRotation?(a, a_rot)
 pp matrixclass.testRotation?(a, matrixclass.rotateCounterclockwise(a))
-pp matrixclass.rotateCounterclockwise(b)
-
-
+pp matrixclass.testRotation?(c, c_rot)
+pp matrixclass.testRotation?(b, b)
