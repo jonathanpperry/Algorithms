edf9a4d5e8448684be0af99560d2c3c767746461
Jonathan Perry <jpper@umich.edu>
2019-03-18 14:33:48 +0900

    Finished up matrix rotation problem

--- Ruby/Matrixrotation.rb
+++ Ruby/Matrixrotation.rb
@@ -2,26 +2,35 @@ require 'pp'
 
 class Matrixrotation
 
-  a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  a_rot = [[3, 6, 9], [2, 5, 8], [1, 4, 7]]
-  b = [[2, 4], [1, 3]]
-  Integer n = a.length # n=3
-  i=0, j=0
-
-  pp a.transpose.reverse
-
-  a.each do |array|
-    array.each do |currElement|
-      puts 'i is ' + i.to_s + ', j is ' + j.to_s
-      # if currElement == a_rot[n-1-j, i]
-      #   puts "Hello, world!"
-      end
-      ++j
-    end
-    ++i
+  def rotateCounterclockwise(arrayToRotate)
+    return arrayToRotate.transpose.reverse
   end
-      # puts 'i is ' + i.to_s + ' and j is ' + j.to_s
-      # if a[i, j] != b[n-1-j, i]
-      #   puts 'failed'
-      # end
-      # puts a[i, j]
+
+  def testRotation?(origArray, rotatedArray)
+    Integer n = origArray.length # n=3
+    i=0
+    origArray.each do |array|
+      j=0
+      array.each do |currElement|
+        # puts 'i is ' + i.to_s + ', j is ' + j.to_s
+        if currElement != rotatedArray[n-1-j][i]
+          return false
+        end
+        j+=1
+      end
+      i+=1
+    end
+    return true
+  end
+
+end
+
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+a_rot = [[3, 6, 9], [2, 5, 8], [1, 4, 7]]
+b = [[2, 4], [1, 3]]
+matrixclass = Matrixrotation.new
+pp matrixclass.testRotation?(a, a_rot)
+pp matrixclass.testRotation?(a, matrixclass.rotateCounterclockwise(a))
+pp matrixclass.rotateCounterclockwise(b)
+
+
